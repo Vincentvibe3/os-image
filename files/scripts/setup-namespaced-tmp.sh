034983d4cf024a53a9c9ba4124b7bdc76fed8a6c
Vincent <vincentvibe3@gmail.com>
2026-09-03 19:59:39 -0400

    remove chcon from namespaced tmp

--- files/scripts/setup-namespaced-tmp.sh
+++ files/scripts/setup-namespaced-tmp.sh
@@ -9,7 +9,7 @@ set -oue pipefail
 mkdir -m 000 /tmp-inst
 mkdir -m 000 /var/tmp/tmp-inst
 
-chcon --reference /tmp /tmp-inst
-chcon --reference /var/tmp /var/tmp/tmp-inst
+# chcon --reference /tmp /tmp-inst
+# chcon --reference /var/tmp /var/tmp/tmp-inst
 
 cat /usr/share/ublue-os/namespaces/temp-namespace.conf >> /etc/security/namespace.conf
