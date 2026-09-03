4f1116cae7e2122af9f22135c94143c27e89dd45
Vincent <vincentvibe3@gmail.com>
2026-09-03 19:42:31 -0400

    fix namespaced temp

--- files/scripts/setup-namespaced-tmp.sh
+++ files/scripts/setup-namespaced-tmp.sh
@@ -1,3 +1,9 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
 
 # Setup polyinstanced /tmp
 mkdir -m 000 /tmp-inst
