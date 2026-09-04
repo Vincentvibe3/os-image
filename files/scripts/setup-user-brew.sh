9b2aeff68bbcd7ff3d2777d7f235e9b8b7858c25
Vincent <vincentvibe3@gmail.com>
2026-09-04 15:22:36 -0400

    adjust namespace files

--- files/scripts/setup-user-brew.sh
+++ files/scripts/setup-user-brew.sh
@@ -24,7 +24,7 @@ mkdir -p -m 000 /var/user-brew
 # mkdir -p -m 000 /var/user-brew
 
 # pam_namespace_helper ignores namespace.d so merge to main config
-# cat /usr/share/ublue-os/namespaces/brew-namespace.conf >> /etc/security/namespace.conf
+cat /usr/share/ublue-os/namespaces/brew-namespace.conf >> /etc/security/namespace.conf
 
-cp /usr/share/ublue-os/namespaces/brew-namespace.conf /etc/security/namespace.d/brew-namespace.conf
+# cp /usr/share/ublue-os/namespaces/brew-namespace.conf /etc/security/namespace.d/brew-namespace.conf
 cat /etc/security/namespace.conf
