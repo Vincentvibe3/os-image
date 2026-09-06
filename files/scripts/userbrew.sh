e3eeb022d0c0310bb658315ef3b00dca0125c0cc
Vincent <vincentvibe3@gmail.com>
2026-09-06 13:13:19 -0400

    add back polyinstantiation

--- files/scripts/userbrew.sh
+++ files/scripts/userbrew.sh
@@ -36,17 +36,18 @@ mkdir -p /var/home/linuxbrew/
 # cp /usr/share/ublue-os/userbrew/generate-brew-dirs.py /usr/libexec/user-brew/generate-brew-dirs
 # chmod +x /usr/libexec/user-brew/generate-brew-dirs
 # cp /usr/share/ublue-os/userbrew/brew-folders-setup.service /etc/systemd/system
+
 # systemctl enable brew-folders-setup.service
 for gdmfile in $(ls /etc/pam.d/gdm*); do
 	if [ $(grep -c "session    required    pam_namespace.so unmnt_remnt ignore_config_error debug" $gdmfile) -ne 0 ]; then
-		# sed -i 's/session    required    pam_namespace.so/session    required    pam_namespace.so unmnt_remnt ignore_config_error debug/' $gdmfile
-		sed -i 's/session    required    pam_namespace.so//' $gdmfile
+		sed -i 's/session    required    pam_namespace.so/session    required    pam_namespace.so unmnt_remnt ignore_config_error debug/' $gdmfile
+		# sed -i 's/session    required    pam_namespace.so//' $gdmfile
 	fi
 done
 # pam_namespace_helper ignores namespace.d so merge to main config
-# cat /usr/share/ublue-os/userbrew/brew-namespace.conf >> /etc/security/namespace.conf
-# cp /usr/share/ublue-os/userbrew/userbrew-namespace.init /etc/security/namespace.d/userbrew-namespace.init
-# chmod +x /etc/security/namespace.d/userbrew-namespace.init
+cat /usr/share/ublue-os/userbrew/brew-namespace.conf >> /etc/security/namespace.conf
+cp /usr/share/ublue-os/userbrew/userbrew.init /etc/security/namespace.d/userbrew.init
+chmod +x /etc/security/namespace.d/userbrew.init
 
 # sed -i 's/exit 0//' /etc/security/namespace.init
 
