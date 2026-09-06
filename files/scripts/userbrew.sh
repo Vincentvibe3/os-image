0ac32de36407e9a588a1fe418b55660bb4e12c0e
Vincent <vincentvibe3@gmail.com>
2026-09-06 10:06:44 -0400

    adjust pam files for gdm

--- files/scripts/userbrew.sh
+++ files/scripts/userbrew.sh
@@ -36,7 +36,11 @@ mkdir -p /var/home/linuxbrew/
 # chmod +x /usr/libexec/user-brew/generate-brew-dirs
 # cp /usr/share/ublue-os/userbrew/brew-folders-setup.service /etc/systemd/system
 # systemctl enable brew-folders-setup.service
-
+for gdmfile in $(ls /etc/pam.d/gdm*); do
+	if [ $(grep -c "session    required    pam_namespace.so unmnt_remnt ignore_config_error debug" $gdmfile) -ne 0 ]; then
+		sed -i 's/session    required    pam_namespace.so/session    required    pam_namespace.so unmnt_remnt ignore_config_error debug/' $gdmfile
+	fi
+done
 # pam_namespace_helper ignores namespace.d so merge to main config
 cat /usr/share/ublue-os/userbrew/brew-namespace.conf >> /etc/security/namespace.conf
 # cp /usr/share/ublue-os/userbrew/userbrew-namespace.init /etc/security/namespace.d/userbrew-namespace.init
