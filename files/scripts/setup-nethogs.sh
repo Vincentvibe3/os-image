90e168e081333acb864bcc5d0e5ff904293d7c1d
Vincent <vincentvibe3@gmail.com>
2026-09-06 23:26:48 -0400

    change nethogs setup

--- files/scripts/setup-nethogs.sh
+++ files/scripts/setup-nethogs.sh
@@ -5,5 +5,4 @@
 # builds actually ran successfully without any errors!
 set -oue pipefail
 
-sudo setcap "cap_net_admin,cap_net_raw,cap_dac_read_search,cap_sys_ptrace+pe" $(readlink -f $(which nethogs))
-sudo ln -s $(which nethogs) /usr/local/sbin/
+setcap "cap_net_admin,cap_net_raw,cap_dac_read_search,cap_sys_ptrace+pe" $(readlink -f $(which nethogs))
